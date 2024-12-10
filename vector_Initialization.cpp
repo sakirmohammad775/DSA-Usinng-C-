f37eb96bc66a076b422c8eb117dd0b256aea0cf6
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-12-10 09:32:41 +0600

    add vector initialization

--- vector_Initialization.cpp
+++ vector_Initialization.cpp
@@ -1,7 +1,19 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    vector<int>v(100);
-    cout<<v.size()<<endl;
+int main()
+{
+    // vector<int>v(100);
+    // vector<int>v(10,3);
+    // vector<int>v2(v);
+    vector<int>v;
+    v.push_back(1);
+    v.push_back(20);
+    v.push_back(3);
+    cout << v.size() << endl;
+    v.clear();
+    cout << v.size() << endl;
+    cout << v[1] << endl;
+
+
     return 0;
 }
